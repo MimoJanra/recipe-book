259eaaa4869a4d9b2c236b718c6d0a88a3ed1c05
aalexeev <a.alexeev@fbs.com>
2019-12-18 15:07:02 +0300

    add navbar and simply card

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
-export class AppComponent {
+
+export class HeaderComponent {
   title = 'RecipeBook';
 }
